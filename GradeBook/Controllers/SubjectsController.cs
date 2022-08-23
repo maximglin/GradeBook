96b21b9ddffed746faf82e8c64e55ffd5e92841f
Максим Глинкин <maximglin05@yandex.ru>
2022-08-23 17:58:33 +0300

    qweqweqwe

---
+++ GradeBook/Controllers/SubjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GradeBook.Controllers
+{
+    public class SubjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
